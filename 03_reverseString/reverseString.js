91b87a0351443a83a10ed51a57ca5ee5a89d7310
Kenneth Ornieta <me@kennethornieta.com>
2022-07-30 00:30:40 +0800

    js exercises answers

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,25 @@
-const reverseString = function() {
+const reverseString = function(str) {
+
+
+  //Split string
+  let splitString = str.split("");
+  // string becomes an array of characters
+  // "lodicake" =====> ["l", "o", "d", "i", "c", "a", "k", "e"]
+
+  let reverseArray = splitString.reverse();
+  // ["e", "k", "a", "c", "i", "d", "o", "l"]
+
+  let joinArray = reverseArray.join(""); 
+  // reversed array becomes a string again
+  // ["e", "k", "a", "c", "i", "d", "o", "l"] =====> "ekacidol"
+
+  return joinArray;
+
+
+  // Short method
+  // return str.split("").reverse().join("");
+
+
 
 };
 
